3990c15157cd4e3390162712ff8242e66facb845
Prashant <prashantk.tps@gmail.com>
2019-03-29 00:59:21 +0530

    adding database

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ const fs=require('fs');
 const bodyParser=require('body-parser');
 const cors=require('cors');
 const app=express();
+var knex = require('knex')({
+  client: 'pg',
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+  }
+});
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -11,26 +18,43 @@ app.post("/email",(req,res)=>
 	var mailformat = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 	if(req.body.email.match(mailformat))
 	{
-		fs.readFile('email.txt',function(err,data)
-		{
-			if(err)
-				res.json("failed")
-			if(data.indexOf(req.body.email)>=0)
-				res.json("exists")
-			else{
-		
-					fs.open('email.txt', 'a', 666, function( e, id ) {
-						if(e)
-							res.json("failed");
-					   else
-					   	{fs.write( id, req.body.email + "\r\n", null, 'utf8', function(){
-				    	fs.close(id, function(){
-							res.json("success");
-					    });
-					   });}
-					  });
-				}
+		knex.select('email').from('email').where('email','=',req.body.email)
+		.then(data=>{
+			if(data.length===0)
+			{
+				knex('email').insert({email: req.body.email});
+				res.json("success")
+			}
+			else
+			{
+				res.json("exits");
+			}
 		});
+
+
+
+
+
+		// fs.readFile('email.txt',function(err,data)
+		// {
+		// 	if(err)
+		// 		res.json("failed")
+		// 	if(data.indexOf(req.body.email)>=0)
+		// 		res.json("exists")
+		// 	else{
+		
+		// 			fs.open('email.txt', 'a', 666, function( e, id ) {
+		// 				if(e)
+		// 					res.json("failed");
+		// 			   else
+		// 			   	{fs.write( id, req.body.email + "\r\n", null, 'utf8', function(){
+		// 		    	fs.close(id, function(){
+		// 					res.json("success");
+		// 			    });
+		// 			   });}
+		// 			  });
+		// 		}
+		// });
 	}
 	else
 		res.json("invalid");	
